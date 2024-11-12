1fd572824ee4ae150aa835bb86e2528fd73aa15b
jspmern <jspmern@gmail.com>
2024-11-12 17:29:55 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from app.forms import *
+
+def registration(request):
+    EUFO=UserForm()
+    EPFO=ProfileForm()
+    d={'EUFO':EUFO,'EPFO':EPFO}
+    return render(request,'registration.html',d)
