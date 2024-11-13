adbdf39da6396cdbdb12e8bf8468d40c8b66c1bc
jspmern <jspmern@gmail.com>
2024-11-13 16:52:03 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -2,9 +2,35 @@ from django.shortcuts import render
 
 # Create your views here.
 from app.forms import *
+from django.http import HttpResponse
 
 def registration(request):
     EUFO=UserForm()
     EPFO=ProfileForm()
     d={'EUFO':EUFO,'EPFO':EPFO}
+
+    if request.method=="POST" and request.FILES:
+        NMUFDO=UserForm(request.POST)
+        NMPFDO=ProfileForm(request.POST,request.FILES)
+        if NMUFDO.is_valid() and NMPFDO.is_valid():
+            
+            MUFDO=NMUFDO.save(commit=False)
+            pw=NMUFDO.cleaned_data['password']
+            MUFDO.set_password(pw)
+            MUFDO.save()
+
+            MPFDO=NMPFDO.save(commit=False)
+            MPFDO.username=MUFDO
+            MPFDO.save()
+
+            return HttpResponse('REgistration is Successfull')
+        else:
+            return HttpResponse('Invalid Data')
+
+
+
+
+
+
+
     return render(request,'registration.html',d)
