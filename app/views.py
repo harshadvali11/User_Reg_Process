dba2d465625b5b72f4491b758b4eaf1191593364
jspmern <jspmern@gmail.com>
2024-11-15 17:22:54 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 from app.forms import *
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
+from django.urls import reverse
 from django.core.mail import send_mail
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+
 
 def registration(request):
     EUFO=UserForm()
@@ -41,3 +45,37 @@ def registration(request):
 
 
     return render(request,'registration.html',d)
+
+
+def home(request):
+    if request.session.get('username'):
+        username=request.session.get('username')
+        d={'username':username}
+        return render(request,'home.html',d)
+
+    return render(request,'home.html')
+
+def user_login(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        AUO=authenticate(username=username,password=password)
+        if AUO:
+            if AUO.is_active:
+                login(request,AUO)
+                request.session['username']=username
+                return HttpResponseRedirect(reverse('home'))
+            else:
+                return HttpResponse('Not an Active User')
+        return HttpResponse('Invalid Credentials')
+        
+
+    return render(request,'user_login.html')
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('home'))
+
+
